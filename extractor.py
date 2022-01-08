a0adf397aa0c6392984c32384bc0c2c1766f5ab0
Singh Kiran <kiran.singh@student.uhasselt.be>
2022-01-08 19:33:05 +0100

    Sat,  8 Jan, 2022 19:33:05

--- extractor.py
+++ extractor.py
@@ -1,6 +1,10 @@
+import textwrap
+from fpdf import FPDF
 from typing import List, Tuple
+import re
 
 import fitz
+from pyparsing import unicode
 
 
 def _parse_highlight(annot: fitz.Annot, wordlist: List[Tuple[float, float, float, float, str, int, int, int]]) -> str:
@@ -18,7 +22,9 @@ def _parse_highlight(annot: fitz.Annot, wordlist: List[Tuple[float, float, float
 
 
 def handle_page(page):
-    wordlist = page.getText("words")  # list of words on page
+    wordlist = page.getText(
+        'words', flags=fitz.TEXT_PRESERVE_LIGATURES | fitz.TEXT_INHIBIT_SPACES)
+
     wordlist.sort(key=lambda w: (w[3], w[0]))  # ascending y, then x
 
     highlights = []
@@ -40,5 +46,55 @@ def main(filepath: str) -> List:
     return highlights
 
 
+def text_to_pdf(filename, data):
+    a4_width_mm = 210
+    pt_to_mm = 0.35
+    fontsize_pt = 10
+    fontsize_mm = fontsize_pt * pt_to_mm
+    margin_bottom_mm = 10
+    character_width_mm = 7 * pt_to_mm
+    width_text = a4_width_mm / character_width_mm
+
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.set_auto_page_break(True, margin=margin_bottom_mm)
+    pdf.add_page()
+    pdf.set_font(family='Courier', size=fontsize_pt)
+    splitted = text.split('\n')
+
+    for i in range(len(data)):
+        result = data[i]
+        result.replace("\xa0", "")
+        result = re.sub(r'[^\x00-\x7f]', r'', data[i])
+        lines = textwrap.wrap(result, width_text)
+
+        if len(lines) == 0:
+            pdf.ln()
+
+        for wrap in lines:
+            pdf.cell(0, fontsize_mm, wrap, ln=1)
+
+    pdf.output(filename, 'F')
+
+def export(data):
+
+    pdf = FPDF()
+
+    # Add a page
+    pdf.add_page()
+
+    # set style and size of font
+    # that you want in the pdf
+    pdf.set_font("Arial", size=15)
+
+
+        # pdf.cell(50, 20, stxt, 0, 2, 'L')
+        # create a cell
+        pdf.cell(200, 10, txt=result,
+                 ln=i, align='l')
+
+    # save the pdf with name .pdf
+    pdf.output("GFG.pdf")
+
+
 if __name__ == "__main__":
-    print(main("example.pdf"))
+    export(main("example.pdf"))
