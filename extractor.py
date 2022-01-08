fd658b474f236e8a699645abf82570df0b862b0c
Singh Kiran <kiran.singh@student.uhasselt.be>
2022-01-08 18:59:27 +0100

    update

---
+++ extractor.py
@@ -0,0 +1,44 @@
+from typing import List, Tuple
+
+import fitz
+
+
+def _parse_highlight(annot: fitz.Annot, wordlist: List[Tuple[float, float, float, float, str, int, int, int]]) -> str:
+    points = annot.vertices
+    quad_count = int(len(points) / 4)
+    sentences = []
+    for i in range(quad_count):
+        # where the highlighted part is
+        r = fitz.Quad(points[i * 4: i * 4 + 4]).rect
+
+        words = [w for w in wordlist if fitz.Rect(w[:4]).intersects(r)]
+        sentences.append(" ".join(w[4] for w in words))
+    sentence = " ".join(sentences)
+    return sentence
+
+
+def handle_page(page):
+    wordlist = page.getText("words")  # list of words on page
+    wordlist.sort(key=lambda w: (w[3], w[0]))  # ascending y, then x
+
+    highlights = []
+    annot = page.firstAnnot
+    while annot:
+        if annot.type[0] == 8:
+            highlights.append(_parse_highlight(annot, wordlist))
+        annot = annot.next
+    return highlights
+
+
+def main(filepath: str) -> List:
+    doc = fitz.open(filepath)
+
+    highlights = []
+    for page in doc:
+        highlights += handle_page(page)
+
+    return highlights
+
+
+if __name__ == "__main__":
+    print(main("example.pdf"))
