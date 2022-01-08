cd5d8977d9fbfc47e70ec5bd8fa524fe592982bb
Singh Kiran <kiran.singh@student.uhasselt.be>
2022-01-08 19:40:29 +0100

    working extractor

--- extractor.py
+++ extractor.py
@@ -49,7 +49,7 @@ def main(filepath: str) -> List:
 def text_to_pdf(filename, data):
     a4_width_mm = 210
     pt_to_mm = 0.35
-    fontsize_pt = 10
+    fontsize_pt = 13
     fontsize_mm = fontsize_pt * pt_to_mm
     margin_bottom_mm = 10
     character_width_mm = 7 * pt_to_mm
@@ -58,8 +58,7 @@ def text_to_pdf(filename, data):
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.set_auto_page_break(True, margin=margin_bottom_mm)
     pdf.add_page()
-    pdf.set_font(family='Courier', size=fontsize_pt)
-    splitted = text.split('\n')
+    pdf.set_font(family='Arial', size=fontsize_pt)
 
     for i in range(len(data)):
         result = data[i]
@@ -72,29 +71,12 @@ def text_to_pdf(filename, data):
 
         for wrap in lines:
             pdf.cell(0, fontsize_mm, wrap, ln=1)
+        pdf.ln()
+        pdf.ln()
+
 
     pdf.output(filename, 'F')
 
-def export(data):
-
-    pdf = FPDF()
-
-    # Add a page
-    pdf.add_page()
-
-    # set style and size of font
-    # that you want in the pdf
-    pdf.set_font("Arial", size=15)
-
-
-        # pdf.cell(50, 20, stxt, 0, 2, 'L')
-        # create a cell
-        pdf.cell(200, 10, txt=result,
-                 ln=i, align='l')
-
-    # save the pdf with name .pdf
-    pdf.output("GFG.pdf")
-
-
 if __name__ == "__main__":
-    export(main("example.pdf"))
+
+    text_to_pdf("export.pdf",main("example.pdf"))
